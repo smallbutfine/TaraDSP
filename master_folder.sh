8534d45ff1530cd7081dd034bbb6c680e36f27b2
photophobic martin <80461402+smallbutfine@users.noreply.github.com>
2026-05-22 05:58:07 +0200

    Update master_folder.sh

--- master_folder.sh
+++ master_folder.sh
@@ -1,6 +1,6 @@
 #!binbash
 # Batch process a whole folder for mastering only
 for f in .raw_samples.wav; do
-    .IRConvolverPro-CLI -i1 $f -o .mastered$(basename $f) -r 48000 -b 24 --agc
+    .TaraDSP -i1 $f -o .mastered$(basename $f) -r 48000 -b 24 --agc
 done
 echo All samples mastered and resampled.
